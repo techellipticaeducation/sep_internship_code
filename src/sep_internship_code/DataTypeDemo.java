dca649f52baba165da118448eec6161164d9d03e
Teche <techellipticaeducation@gmail.com>
2023-09-30 12:23:01 -0500

    day3

---
+++ src/sep_internship_code/DataTypeDemo.java
@@ -0,0 +1,51 @@
+package sep_internship_code;
+
+public class DataTypeDemo {
+	
+	public static void main(String[] args) {
+		
+		// casting concept  - converting one type into other
+		
+		// numerical - default int
+		
+		//byte h = 137;
+		
+		
+//		byte b1 = (byte)300; // 2 power 8 = 256
+//		System.out.println(b1);
+	
+		
+//      1 byte = 8 bit 		
+//		-128 to 127
+// 		-(2^7) to (2^7 - 1) 
+		
+//		int i = 0b10101011; 
+		// 0  - Octal number,
+		// 0X - Hexadecimal
+		// 0b - binary
+//		System.out.println(i);
+	
+//		float g = 12.5f;
+//		System.out.println(g);
+//		
+//		double db  = 12.78d;
+//		System.out.println(db);
+//		
+//		boolean h1 = true;
+		
+//		char c = 'f';
+//		int ca = (int)c;
+//		System.out.println(ca);
+		
+//		int k = 87;
+//		char m = (char) k;
+//		System.out.println(m);
+		
+//		Unicode
+//		char h1 = '\u0BA3';
+//		System.out.println(h1);
+		
+		
+	}
+	
+}
---
+++ src/sep_internship_code/OperatorDemo.java
@@ -0,0 +1,34 @@
+package sep_internship_code;
+
+public class OperatorDemo {
+	public static void main(String[] args) {
+		// Arithmatic Operator
+		int i = 10;
+		int j = 7;
+		
+		System.out.println(i+j); // 17
+		System.out.println(i-j); // 3
+		System.out.println(i*j); // 70
+		System.out.println((float)i/(float)j); // 1.42
+		System.out.println(i%j); // 3
+		
+		
+//		1 = odd
+//		2 = even
+//		3 = odd
+//		4 = even
+//		5 = odd
+//		6 = even
+		
+		// write a code to find out if value is odd or even
+		int k = 3;
+		if(k%2 == 0) {
+			System.out.println("Even");
+		}else {
+			System.out.println("Odd");
+		}
+		
+ 		
+		
+	}
+}
